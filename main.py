dfb299053c0a626d65fb3d864d1388ec728db762
mohtasimhadi <mohtasimhadi@gmail.com>
2024-07-29 13:48:23 -0500

    subscriber

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+import time
 import depthai as dai
 import subprocess
 
 def main():
     devices = dai.Device.getAllAvailableDevices()
     port_no = 5500
+
     for device in devices:
         print("Connected cameras:", device.mxid)
         subprocess.Popen(['gnome-terminal', '--', 'bash', '-c', f'python3 depthai_cam.py --mxid {device.mxid} --port_no {port_no}; exec bash'])
         port_no += 1
+
+    time.sleep(15)
+    print("Hello World!")
+
 
 if __name__ == "__main__":
     main()
--- subscribers.py
+++ subscribers.py
@@ -1,25 +1,27 @@
-import os
-import cv2
-import pickle
-import zmq
-from modules.depthai.utils import get_files
+import os, cv2, pickle, zmq, argparse
+from modules.depthai.utils import get_files, write_imu
 
 def main():
+    parser = argparse.ArgumentParser(description="Data Collection Subscriber")
+    # parser.add_argument('--device_count', type=int, required=True, help="Number of Camera")
+    parser.add_argument('--port_no', type=str, required=True, help="Publisher Port No")
+    args = parser.parse_args()
+
     context = zmq.Context()
     subscriber = context.socket(zmq.SUB)
-    subscriber.connect("tcp://localhost:5555")
+    subscriber.connect(f"tcp://localhost:{args.port_no}")
     subscriber.setsockopt_string(zmq.SUBSCRIBE, "")
 
     file_color, file_monoL, file_monoR, file_imus = get_files("out", "thread")
 
     while True:
-        print("Dhukse!")
         message = pickle.loads(subscriber.recv())
         file_color.write(message["color"])
         file_monoL.write(message["monoL"])
         file_monoR.write(message["monoR"])
-        depth_file_name = f'{"thread"}_depth/' +str(message['depth']['sequence']) + f"_{str(message['depth']['timestamp'])}.png"
+        depth_file_name = f'depth/' +str(message['depth']['sequence']) + f"_{str(message['depth']['timestamp'])}.png"
         cv2.imwrite(os.path.join("out", depth_file_name), message['depth']['frame'])
+        write_imu(file_imus=file_imus, imu_message=message["imu"])
 
 
 if __name__ == "__main__":
