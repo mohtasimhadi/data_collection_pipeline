4cb11e0c0ea11c49753c0adf6b9f36a6ade0523a
mohtasimhadi <mohtasimhadi@gmail.com>
2024-07-29 10:59:03 -0500

    initial subscriber

---
+++ subscribers.py
@@ -0,0 +1,26 @@
+import os
+import cv2
+import pickle
+import zmq
+from modules.depthai.utils import get_files
+
+def main():
+    context = zmq.Context()
+    subscriber = context.socket(zmq.SUB)
+    subscriber.connect("tcp://localhost:5555")
+    subscriber.setsockopt_string(zmq.SUBSCRIBE, "")
+
+    file_color, file_monoL, file_monoR, file_imus = get_files("out", "thread")
+
+    while True:
+        print("Dhukse!")
+        message = pickle.loads(subscriber.recv())
+        file_color.write(message["color"])
+        file_monoL.write(message["monoL"])
+        file_monoR.write(message["monoR"])
+        depth_file_name = f'{"thread"}_depth/' +str(message['depth']['sequence']) + f"_{str(message['depth']['timestamp'])}.png"
+        cv2.imwrite(os.path.join("out", depth_file_name), message['depth']['frame'])
+
+
+if __name__ == "__main__":
+    main()
